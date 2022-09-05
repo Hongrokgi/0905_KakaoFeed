526f79fb735413f17bb116773a0ce3e3a7cc2035
Hongrokgi <Hongrokgi@naver.com>
2022-09-05 17:08:16 +0900

    Kakao Feed Message 전송 성공(나에게 보내기)

--- src/main/java/com/demo/kakaoMessage/controller/MessageController.java
+++ src/main/java/com/demo/kakaoMessage/controller/MessageController.java
@@ -1,9 +1,28 @@
 package com.demo.kakaoMessage.controller;
 
+import com.demo.kakaoMessage.service.AuthService;
+import com.demo.kakaoMessage.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
 
+    @Autowired
+    AuthService authService;
+
+    @Autowired
+    MessageService messageService;
+
+
+    @GetMapping("/")
+    public String serviceStart(String code) {
+       if(authService.getKakaoAuthToken(code)) {
+            messageService.sendMessage(AuthService.authToken);
+            return "메시지 전송 성공";
+       }else {
+           return "토큰 발급 실패";
+       }
+     }
 }
