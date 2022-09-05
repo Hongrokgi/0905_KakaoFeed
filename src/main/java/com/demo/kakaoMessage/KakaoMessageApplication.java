fbfbc64ff971fb34ee33be6ce84027961ca39d3c
Hongrokgi <Hongrokgi@naver.com>
2022-09-05 10:28:25 +0900

    Create Repository

---
+++ src/main/java/com/demo/kakaoMessage/KakaoMessageApplication.java
@@ -0,0 +1,13 @@
+package com.demo.kakaoMessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/kakaoMessage/KakaoMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.kakaoMessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
