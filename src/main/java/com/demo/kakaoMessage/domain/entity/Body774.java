5183c0f745393d95e14bfb99d3d63caff0939e7e
Hongrokgi <Hongrokgi@naver.com>
2022-09-06 18:29:12 +0900

    0906 사용자 정의 템플릿

---
+++ src/main/java/com/demo/kakaoMessage/domain/entity/Body774.java
@@ -0,0 +1,49 @@
+package com.demo.kakaoMessage.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class Body774 {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String cameraName;
+
+    @Column(nullable = false)
+    private String channel;
+
+    @Column(nullable = false)
+    private String deviceId;
+
+    @Column(nullable = false)
+    private String eventsType;
+
+    @Column(nullable = false)
+    private String snapId;
+
+    @Column(nullable = false)
+    private String snapPath;
+
+    @Column(nullable = false)
+    private String trigger;
+
+    @Builder
+    public Body774(String cameraName, String channel, String deviceId, String eventsType, String snapId, String snapPath, String trigger) {
+        this.cameraName = cameraName;
+        this.channel = channel;
+        this.deviceId = deviceId;
+        this.eventsType = eventsType;
+        this.snapId = snapId;
+        this.snapPath = snapPath;
+        this.trigger = trigger;
+    }
+}
